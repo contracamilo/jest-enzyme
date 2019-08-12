d9715e5f47aa3dff134b169b1e90fcc17f66ed3a
camilo.rivera <camilo.rivera@globant.com>
2019-08-12 13:35:49 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
+    
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+
+test('render without failures', () => {
+
+})
+test('render incremen button', () => {
+
+})
+test('render counter display', () => {
+
+})
+test('counters start at 0', () => {
+
+})
+test('clicking button increments counter', () => {
+
+})
